2bb322bd67105d23343ecaedaaf3d31e684e79f7
pierre <pierre.garcia@epita.fr>
2022-11-02 21:20:43 +0100

    First part completed

--- expense.py
+++ expense.py
@@ -1,4 +1,6 @@
+import csv
 from PyInquirer import prompt
+from user import makelistuser , makelistfriends
 
 expense_questions = [
     {
@@ -12,18 +14,50 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": makelistuser()
     },
-
 ]
 
+expense_users = []
 
 
 def new_expense(*args):
+    expense_questions = [
+    {
+        "type":"input",
+        "name":"amount",
+        "message":"New Expense - Amount: ",
+    },
+    {
+        "type":"input",
+        "name":"label",
+        "message":"New Expense - Label: ",
+    },
+    {
+        "type":"list",
+        "name":"spender",
+        "message":"New Expense - Spender: ",
+        "choices": makelistuser()
+    },
+    {
+        "type":"checkbox",
+        "name":"friends_involved",
+        "message":"Who did you pay for ?:",
+        "choices": makelistfriends() 
+    },
+    ]
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('expense_report.csv', 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=infos.keys())
+        writer.writeheader()
+        writer.writerow(infos)
+    
+    print(infos)
+    print("it means for each person : "  , int(infos["amount"]) / len(infos["friends_involved"]))
+
     print("Expense Added !")
     return True
 
--- user.py
+++ user.py
@@ -1,8 +1,35 @@
+import csv
 from PyInquirer import prompt
-user_questions = [
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"Comment s'appelle ce bg ?:",
+    },
 ]
 
-def add_user():
-    # This function should create a new user, asking for its name
-    return
+def add_user(*args):
+    infos = prompt(user_questions)
+    print(infos)
+    print("User Added !")
+    with open('users.csv', 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=infos.keys())
+        writer.writerow(infos)
+    return True
+
+def makelistuser():
+    with open('users.csv', 'r') as f:
+        listuser = f.read().splitlines()
+    return listuser
+
+def makelistfriends():
+    with open('users.csv', 'r') as f:
+        listuser = f.read().splitlines() 
+    res = []
+    for element in listuser:
+        my_dict = dict()
+        my_dict.update({"name" :  element})
+        res.append(my_dict)
+    
+    return res
